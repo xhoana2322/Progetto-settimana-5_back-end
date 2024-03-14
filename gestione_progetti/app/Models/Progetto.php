de5cd140f85a7418417b94b4e81cbae2bda5471e
xhoana2322 <xhoana.beqiri23@gmail.com>
2024-03-14 10:29:06 +0100

    Progetto-settimana-5_back-end

---
+++ gestione_progetti/app/Models/Progetto.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Progetto extends Model
+{
+    use HasFactory;
+}
